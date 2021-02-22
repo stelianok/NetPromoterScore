2212375c9e64ab32378db4fc7a5d54f4c9bb0d2b
Kauã Steliano <stelianok@gmail.com>
2021-02-22 11:06:22 -0300

    added some routes

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,15 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const app = express();
+
+app.get('/', async (request: Request, response: Response) => {
+    return response.json({ message: 'Hello wrld!!!!' })
+});
+
+app.post('/', async (request: Request, response: Response) => {
+    return response.json({ message: 'Dados salvos com sucesso' })
+});
+
 
 app.listen(3333, () => {
     console.log('Listening on Port 3333');
