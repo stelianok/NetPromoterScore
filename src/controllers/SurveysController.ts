41340eedc2a88dd62b704402eea508adb3c0e623
Kauã Steliano <stelianok@gmail.com>
2021-02-24 08:34:15 -0300

    Created SurveysController

---
+++ src/controllers/SurveysController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import { getCustomRepository } from 'typeorm';
+import SurveysRepository from '../repositories/SurveysRepository';
+
+
+class SurveysController {
+
+    async create(request: Request, response: Response) {
+        const { title, description } = request.body;
+        const surveysRepository = getCustomRepository(SurveysRepository);
+
+        const survey = surveysRepository.create({ title, description });
+
+        await surveysRepository.save(survey);
+    }
+}
+export default SurveysController;
