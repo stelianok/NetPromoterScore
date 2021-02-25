1ad630bcb0d57f8f8e316df7cfb48012000cd53f
Kauã Steliano <stelianok@gmail.com>
2021-02-25 10:26:07 -0300

    Created send mail feature

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -3,6 +3,7 @@ import { getCustomRepository } from 'typeorm';
 import SurveysRepository from '../repositories/SurveysRepository';
 import SurveysUsersRepository from '../repositories/SurveysUsersRepository';
 import UsersRepository from '../repositories/UsersRepository';
+import SendMailService from '../services/SendMailService';
 
 
 class SendMailController {
@@ -20,21 +21,22 @@ class SendMailController {
             return response.status(400).json({ error: "User does not exists" });
         }
 
-        const surveyAlreadyExists = await surveysRepository.findOne({ id: survey_id });
+        const survey = await surveysRepository.findOne({ id: survey_id });
 
-        if (!surveyAlreadyExists) {
+        if (!survey) {
             return response.status(400).json({ error: "Survey does not exist!" })
         }
 
         const surveyUser = surveysUsersRepository.create({
             user_id: userAlreadyExists.id,
-            survey_id: surveyAlreadyExists.id,
+            survey_id: survey.id,
         });
 
         await surveysUsersRepository.save(surveyUser);
 
+        await SendMailService.execute(email, survey.title, survey.description);
         return response.json(surveyUser);
     }
-
 }
+
 export default SendMailController;
