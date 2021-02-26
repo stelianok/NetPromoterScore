ea6277789687f6833e30733d5c19092318f5dfcd
Kauã Steliano <stelianok@gmail.com>
2021-02-26 08:52:50 -0300

    Created answers route.

---
+++ src/controllers/AnswerController.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import SurveysUsersRepository from "../repositories/SurveysUsersRepository";
+
+export default class AnswerController {
+
+    async execute(request: Request, response: Response) {
+        const { value } = request.params;
+        const { u } = request.query;
+
+        const surveysUsersRepository = getCustomRepository(SurveysUsersRepository);
+
+        const surveyUser = await surveysUsersRepository.findOne({
+            id: String(u),
+        });
+
+        if (!surveyUser) {
+            return response.status(400).json({
+                error: "Survey user does not exist"
+            });
+        }
+
+        surveyUser.value = Number(value);
+
+        await surveysUsersRepository.save(surveyUser);
+
+        return response.json(surveyUser);
+    }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import AnswerController from './controllers/AnswerController';
 import SendMailController from './controllers/SendMailController';
 import SurveysController from './controllers/SurveysController';
 import { UserController } from './controllers/UsersController';
@@ -9,11 +10,16 @@ const userController = new UserController();
 const surveysController = new SurveysController();
 const sendMailController = new SendMailController();
 
+const answerController = new AnswerController();
+
 router.post('/users', userController.create);
 
 router.get('/surveys', surveysController.show);
 router.post('/surveys', surveysController.create);
 
 router.post('/sendMail', sendMailController.execute);
+
+router.get('/answers/:value', answerController.execute);
+
 
 export default router;
